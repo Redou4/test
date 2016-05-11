3bff4111e3455c059a7c46acbad61609852b4bb1
Redou4 <redwan.dd@gmail.com>
2016-05-11 15:01:17 +0200

    modif suppression pavé

--- article.php
+++ article.php
@@ -2,22 +2,6 @@
 require ("./class/PDOsql.class.php");
 
 
-$connexion->connect= new PDOsql(); 
-$sql = "SELECT * FROM article ORDER BY id DESC";
-//$opt = array();
-$value = $connexion->connect->query($sql);
-
-
-$list_articles = array();
-$i = 0;
-while($data = $value->fetch()){
-    $list_articles[$i]['id'] = $data['id'];
-    $list_articles[$i]['titre'] = $data['titre'];
-    $list_articles[$i]['texte'] = $data['texte'];
-    $list_articles[$i]['date'] = $data['date'];
-    $list_articles[$i]['tag'] = $data['tag'];
-    $i++;
-}
 $connexion=null;
 
 // On lance Smarty
